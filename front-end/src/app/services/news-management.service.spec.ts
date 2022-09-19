3e7bfa2e8f3391ca48896dc4b19c0326b067ccd3
jlcs3 <jlcs3@cin.ufpe.br>
2022-09-19 17:19:34 -0300

    added more news-managment service tests

--- front-end/src/app/services/news-management.service.spec.ts
+++ front-end/src/app/services/news-management.service.spec.ts
@@ -1,7 +1,7 @@
 import { TestBed } from '@angular/core/testing'
 import { NewsManagementService } from './news-management.service'
 import { HttpClient, HttpClientModule } from '@angular/common/http'
-import { ApiResponse, emptyNews } from '../../../../common/types'
+import { ApiResponse, emptyComment, emptyNews } from '../../../../common/types'
 import { firstValueFrom } from 'rxjs'
 
 jasmine.getEnv().configure({ random: false })
@@ -69,6 +69,51 @@ fdescribe('NewsManagementService', () => {
         expect(spy).toHaveBeenCalledWith('http://localhost:3000/newspage/1/5')
         expect(response.status).toBe(200)
         expect(response.result).not.toBeUndefined()
+    })
+
+    it('Service should be able to ADD A LIKE on news', async () => {
+        let spy = spyOn(http, 'post').and.callThrough()
+
+        let response: ApiResponse = await firstValueFrom(service.addLike('123', '123'))
+
+        expect(spy).toHaveBeenCalledWith('http://localhost:3000/newsadd/like', { newsId: '123', authorLikeId: '123' })
+        expect(response.status).toBe(200)
+    })
+
+    it('Service should be able to ADD A COMMENT on news', async () => {
+        let spy = spyOn(http, 'post').and.callThrough()
+
+        let response: ApiResponse = await firstValueFrom(service.addComment('123', emptyComment('123', '123')))
+
+        expect(spy).toHaveBeenCalledWith('http://localhost:3000/newsadd/comment/123', emptyComment('123', '123'))
+        expect(response.status).toBe(200)
+    })
+
+    it('Service should be able to ADD A LIKE on a comment', async () => {
+        let spy = spyOn(http, 'post').and.callThrough()
+
+        let response: ApiResponse = await firstValueFrom(service.addLikeInComment('123', '123', '123'))
+
+        expect(spy).toHaveBeenCalledWith('http://localhost:3000/news/commentsadd/like', { newsId: '123', commentId: '123', authorLikeId: '123' })
+        expect(response.status).toBe(200)
+    })
+
+    it('Service should be able to ADD A DISLIKE on a comment', async () => {
+        let spy = spyOn(http, 'post').and.callThrough()
+
+        let response: ApiResponse = await firstValueFrom(service.addLikeInComment('123', '123', '123'))
+
+        expect(spy).toHaveBeenCalledWith('http://localhost:3000/news/commentsadd/dislike', { newsId: '123', commentId: '123', authorLikeId: '123' })
+        expect(response.status).toBe(200)
+    })
+
+    it('Service should be able to ADD A VIEW on a news', async () => {
+        let spy = spyOn(http, 'post').and.callThrough()
+
+        let response: ApiResponse = await firstValueFrom(service.addView('123'))
+
+        expect(spy).toHaveBeenCalledWith('http://localhost:3000/newsadd/view', { newsId: '123', commentId: '123', authorLikeId: '123' })
+        expect(response.status).toBe(200)
     })
 
     it('Service should be able to EDIT a news', async () => {
