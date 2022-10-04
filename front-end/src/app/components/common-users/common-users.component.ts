a732d397c7543fa79d7d0c3f139fd7cca8d7ca51
mvbs3 <mvbs3@cin.ufpe.br>
2022-10-03 22:42:43 -0300

    criando componente de common-users

---
+++ front-end/src/app/components/common-users/common-users.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-common-users',
+  templateUrl: './common-users.component.html',
+  styleUrls: ['./common-users.component.css']
+})
+export class CommonUsersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
