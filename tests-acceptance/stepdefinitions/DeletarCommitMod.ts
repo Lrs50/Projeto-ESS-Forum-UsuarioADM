dd3a545b7ac731aaa0584161026761c0ae3437ae
mvbs3 <mvbs3@cin.ufpe.br>
2022-10-17 17:43:50 -0300

    refactoring extract function login deleteCommitMod GUI test

--- tests-acceptance/stepdefinitions/DeletarCommitMod.ts
+++ tests-acceptance/stepdefinitions/DeletarCommitMod.ts
@@ -6,23 +6,22 @@ var { setDefaultTimeout } = require("cucumber");
 
 setDefaultTimeout(60 * 1000);
 
-let sameId = ((elem, id) => elem.element(by.name('cpflist')).getText().then(text => text == id));
-let sameUsername = ((elem, user) => elem.element(by.name('nomelist')).getText().then(text => text == user));
-
-let pAND = ((p,q) => p.then(a => q.then(b => a && b)))
-
-async function criarAluno(name, cpf) {
-    await $("input[name='namebox']").sendKeys(<string> name);
-    await $("input[name='cpfbox']").sendKeys(<string> cpf);
-    await element(by.buttonText('Adicionar')).click();
+async function login(user,passw){
+    await element(by.id("login")).click();
+    await browser.driver.sleep(1000);
+    await preencher('UserSpace',user)
+    await preencher('CpfBox', passw)
+    await element(by.id("logButton")).click();
+    await browser.driver.sleep(1000);
 }
-
-
-async function assertTamanhoEqual(set,n) {
-    await set.then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(n));
+async function preencher(type, content) {
+    if (type == 'UserSpace'){
+        await $("input[name='UserSpace']").sendKeys(<string> content);
+    }else
+        if(type == 'CpfBox'){
+            await $("input[name='CpfBox']").sendKeys(<string> content);
+        }
 }
-
-
 
 defineSupportCode(function ({ Given, When, Then }) {
     Given(/^Eu estou logado como usuário mod "([^\"]*)" com senha "([^\"]*)"$/, async (user,passw) => {
@@ -30,24 +29,14 @@ defineSupportCode(function ({ Given, When, Then }) {
         await browser.driver.sleep(1000);
         await expect(browser.getTitle()).to.eventually.equal('ReviReli');
         if(await element(by.id("login")).isPresent()){
-            await element(by.id("login")).click();
-            await browser.driver.sleep(1000);
-            await $("input[name='UserSpace']").sendKeys(<string> user);
-            await $("input[name='CpfBox']").sendKeys(<string> passw);
-            await element(by.id("logButton")).click();
-            await browser.driver.sleep(1000);
+            await login(user,passw)
             expect (await element(by.id("profileEnter")).isPresent()).to.equal(true)
         }else{
             await element(by.id("profileEnter")).click();
             await browser.driver.sleep(1000);
             await element(by.id("logoutButton")).click();
             await browser.driver.sleep(1000);
-            await element(by.id("login")).click();
-            await browser.driver.sleep(1000);
-            await $("input[name='UserSpace']").sendKeys(<string> user);
-            await $("input[name='CpfBox']").sendKeys(<string> passw);
-            await element(by.id("logButton")).click();
-            await browser.driver.sleep(1000);
+            await login(user,passw)
             expect (await element(by.id("profileEnter")).isPresent()).to.equal(true)
         }
 
